b013bb60eab948e1e2587cfbee551b1425c1aa18
MayThuKyaw237 <maythu.mtkyaw@gmail.com>
2025-08-30 11:49:06 +0700

    Updated

--- Back/chessgame.py
+++ Back/chessgame.py
@@ -4,14 +4,6 @@ from solver import can_still_win, find_complete_solution, find_remaining_solutio
 
 import random
 
-
-class GameState:
-    def __init__(self, size=8):
-        self.board = [['.' for _ in range(size)] for _ in range(size)]
-        self.size = size
-        self.remaining_pieces = {'Q': 1, 'R': 2, 'B': 2, 'P': 8}
-        self.used_positions = set()
-
 def kpos_generator(size): 
     return random.randint(0, size - 1)
 
